122a4c69544b5d6b07ba6662509a86a3cede2e23
samirvithlani <samir.vithlani83955@gmail.com>
2022-04-06 12:58:00 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,75 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'learning';
+
+    title = 'learning';
+
+    flag:boolean = false;
+  
+
+
+    name: string = 'Raj';
+    age: number = 20;
+    isMarried: boolean = false;
+    
+    student : any = {
+        name: 'Raj',
+        age: 30,
+        isMarried: false
+    }
+
+    employee :any = {
+        ename : 'Dhiraj',
+        Salary : 2551531.25,
+        isActive: true
+    }
+
+    subjects :any[] = [
+
+      {
+        name : 'Angular',
+        marks : 80
+      },
+      {
+        name : 'React',
+        marks : 90
+      },
+      {
+        name : 'Vue',
+        marks : 95
+      }
+
+    ]
+
+    bugs:any[] =[
+      {
+        name : 'Login Screen',
+        isClosed : false,
+        severity : 'High'
+    
+      },
+      {
+        name : 'Home Screen issue',
+        isClosed : true,
+        severity : 'Low'
+      },
+      {
+        name : 'Dashboard issue',
+        isClosed : false,
+        severity : 'Low'
+      },
+    ]
+    actors:any[] = [
+      "Amitabh Bachchan",
+      "Rajesh Khanna",
+      "Rajnikanth",
+      "Chiranhivi"
+    ]
+
+
+
+    
+
+
+
 }
